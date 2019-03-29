474650e2ca9db903e629a25da47d059c475a5285
Miaooocat <shihao.miao@mail.mcgill.ca>
2019-03-28 22:10:21 -0700

    Site updated: 2019-03-28 22:10:21

--- dist/music.js
+++ dist/music.js
@@ -10,10 +10,10 @@ const ap = new APlayer({
         cover: 'http://p1.music.126.net/Blb_Gi0AJTWIEBLr189F4A==/18791753232142320.jpg?param=130y130',
       },
       {
-        name: '浮夸',
-        artist: '陈奕迅',
+        name: 'Riverside',
+        artist: 'Agnes Obel',
         url: 'http://www.ytmp3.cn/down/49639.mp3',
-        cover: 'http://p1.music.126.net/Bl1hEdJbMSj5YJsTqUjr-w==/109951163520311175.jpg?param=130y130',
+        cover: 'http://m10.music.126.net/20190329125116/f0d14a8511975823d0cbb5cc0f5caa1d/ymusic/6b9d/72ed/bb9f/ea7622744213d67d9bade159644b42d7.mp3',
       }
     ]
 });
