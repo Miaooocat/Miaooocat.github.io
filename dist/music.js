d45088759847ce8ac84f4aa421f09929a5e74290
Miaooocat <shihao.miao@mail.mcgill.ca>
2019-03-28 22:12:43 -0700

    Site updated: 2019-03-28 22:12:43

--- dist/music.js
+++ dist/music.js
@@ -1,7 +1,8 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: true,
+    autoplay: false,
+
     audio: [
       {
         name: 'Wonderful U',
@@ -12,8 +13,32 @@ const ap = new APlayer({
       {
         name: 'Riverside',
         artist: 'Agnes Obel',
-        url: 'http://www.ytmp3.cn/down/49639.mp3',
-        cover: 'http://m10.music.126.net/20190329125116/f0d14a8511975823d0cbb5cc0f5caa1d/ymusic/6b9d/72ed/bb9f/ea7622744213d67d9bade159644b42d7.mp3',
-      }
+        url: 'http://dl.stream.qqmusic.qq.com/M500000fYyM44I1LwD.mp3?vkey=150DD59BDEE8DF14A218F5B52639B4523F19720C40726D7F5DD6C515E5E1846AB148414656B2B77D74AADB159AA0FDF8CACA01DF808B7A93&guid=5150825362&fromtag=1',
+      },
+      {
+        name: 'Mad World',
+        artist: 'Gary Jules',
+        url: 'http://dl.stream.qqmusic.qq.com/M500002XnZdT1nsaY6.mp3?vkey=97284F51398F491B72FD1D61D4EA8C4221659F27B70FDCF231AF8CA0AC7A679D58CE1984D27C09E6EB811372ADE7D4D39F8D50612C059474&guid=5150825362&fromtag=1',
+      },
+      {
+        name: 'Mad World',
+        artist: '尚雯婕',
+        url: 'http://dl.stream.qqmusic.qq.com/M500001tP9453FRikS.mp3?vkey=32C7CF4A99520045D22DF9DDD1C610A475FFCB107B8D43EC3E4C5062077829EDCE9B6AE6A9549CBD0417C277503C1CD4D6A9AA8FC5900DB4&guid=5150825362&fromtag=1',
+      },
+      {
+        name: '青春多是无奈',
+        artist: '彭青',
+        url: 'http://dl.stream.qqmusic.qq.com/M500000awnFr1Dyp4n.mp3?vkey=40C1724BC3392E6374149E067FBD01994439E854D47C8378B31D2AE06D87B167FBC10ADBCE7E0848467420F3679FFDE9402BE0D14EBE18C9&guid=5150825362&fromtag=1'
+      },
+      {
+        name: '拾荒地图',
+        artist: '黄玠',
+        url: 'http://dl.stream.qqmusic.qq.com/M500000wooCY25pJWO.mp3?vkey=3C224E8C27C6DB95D52596714AA787CB925664AD2C52FD9A6925F95884154D695B25ABF55E483D857E207B469022F47671E7E2D9D92D016D&guid=5150825362&fromtag=1'
+      },
+      {
+        name: '疯子',
+        artist: '许哲佩',
+        url: 'http://dl.stream.qqmusic.qq.com/M5000028OhDd0XNOVP.mp3?vkey=4CB6564DF7E1E837A034310A0A2E9F371B18EDEC24C333E286E65D42920F6284DB253A582CA7B75A901B2ABBD7A1147F7EE1239657634B7B&guid=5150825362&fromtag=1'
+      },
     ]
 });
